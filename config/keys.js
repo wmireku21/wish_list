5cf210083414fb5edad6818ba65dd3abbb19b3d9
wmireku21 <wmireku21@gmail.com>
2020-07-26 17:22:13 -0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://wmireku21:ghanastar!@cluster0.dudnf.mongodb.net/wmireku21?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//bodyparser Middleware
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, {useNewUrlParser:true, useUnifiedTopology:true})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
