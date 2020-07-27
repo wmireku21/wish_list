5978da0957b17a9ab70619f8b2ab5e92398e29d8
wmireku21 <wmireku21@gmail.com>
2020-07-27 18:22:54 -0500

    finished backend

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //bodyparser Middleware
 
 app.use(bodyParser.json());
+
 
 const db = require('./config/keys').mongoURI;
 
@@ -15,6 +18,9 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+// use routes
+
+app.use('/api/items', items);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
